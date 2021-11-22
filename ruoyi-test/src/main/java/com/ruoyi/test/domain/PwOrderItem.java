e30fe8e728b030d877d95cec130d2af0db025252
zkl <1981280278@qq.com>
2021-11-22 13:59:34 +0800

    统计待清运数量

--- ruoyi-test/src/main/java/com/ruoyi/test/domain/PwOrderItem.java
+++ ruoyi-test/src/main/java/com/ruoyi/test/domain/PwOrderItem.java
@@ -1,9 +1,7 @@
 package com.ruoyi.test.domain;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ruoyi.common.annotation.Excel;
 import com.ruoyi.common.core.domain.BaseEntity;
